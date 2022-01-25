de2759b951495ec47fea7e3f66cee91bcf7ad9b9
Adeline Almanzar <almanzar.adeline@gmail.com>
2022-01-25 08:55:24 -0800

    feat: render reviews

--- src/index.js
+++ src/index.js
@@ -6,22 +6,16 @@ const blurb = document.getElementById('blurb')
 const coverImg = document.getElementById('cover')
 const stars = document.getElementById('star-rating')
 const reviewCount = document.getElementById('review-count')
+const reviewListUl = document.getElementById('reviews-list')
 
 fetch('http://localhost:3000/Books')
     .then(res => res.json())
     .then(books => {
-        renderBook(books[0])
-        books.forEach(book => {
-            let newBookLi = document.createElement('li')
-            newBookLi.textContent = book.title
-            newBookLi.id = book.id
-            bookList.append(newBookLi)
-
-            newBookLi.addEventListener('click', e => renderBook(book))
-        })
+        renderBookInfo(books[0])
+        renderBookList(books)
     })
 
-function renderBook(book){
+function renderBookInfo(book){
     let reviewTotal = book.reviews.reduce((total, review) => {
         return total += review.rating },0)
 
@@ -32,4 +26,39 @@ function renderBook(book){
     coverImg.src = book.image
     stars.textContent = reviewTotal/book.reviews.length
     reviewCount.textContent = (book.reviews.length === 1) ? `${book.reviews.length} review` : `${book.reviews.length} reviews`
+    let newDiv = document.createElement('div')
+    removeAllChildren(reviewListUl)
+    renderBookReviews(book)
+}
+
+function removeAllChildren(parent) {
+    while (parent.firstChild) {
+        parent.removeChild(parent.firstChild)
+    }
+}
+
+function renderBookReviews(book) {
+    book.reviews.forEach(review => {
+        let newDiv = document.createElement('div')
+
+        let newRatingP = document.createElement('p')
+        newRatingP.textContent = `${review.rating} stars`
+
+        let newCommentP = document.createElement('p') //TODO: maybe global scope
+        newCommentP.textContent = `Comment: ${review.comment}`
+
+        newDiv.append(newRatingP, newCommentP)
+        reviewListUl.appendChild(newDiv)
+    })
+}
+
+function renderBookList(booksArr) {
+    booksArr.forEach(book => {
+        let newBookLi = document.createElement('li')
+        newBookLi.textContent = book.title
+        newBookLi.id = book.id
+        bookList.append(newBookLi)
+
+        newBookLi.addEventListener('click', e => renderBookInfo(book))
+    })
 }
